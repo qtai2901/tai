b6eba836670e3b4ec286585e4567efd79ab302dc
qtai2901 <67884469+qtai2901@users.noreply.github.com>
2022-04-01 09:26:32 +0700

    Update tai.sh

--- tai.sh
+++ tai.sh
@@ -120,12 +120,7 @@ read -p "Giới hạn thiết bị :" DeviceLimit
   echo "Thiết bị tối đa là: ${DeviceLimit}"
   echo "-------------------------------"
   
-    #CertDomain:
-read -p "CertDomain:" CertDomain
-  [ -z "${CertDomain}" ] && CertDomain="0"
-  echo "-------------------------------"
-  echo "CertDomain: ${CertDomain}"
-  echo "-------------------------------"
+  
   
   
 }
@@ -142,7 +137,7 @@ config_docker() {
 version: '3'
 services: 
   xrayr: 
-    image: crackair/xrayr:latest
+    image: aikocute/xrayr:latest
     volumes:
       - ./config.yml:/etc/XrayR/config.yml # thư mục cấu hình bản đồ
       - ./dns.json:/etc/XrayR/dns.json 
@@ -218,7 +213,7 @@ Nodes:
 EOF
   sed -i "s|NodeID:.*|NodeID: ${node_id}|" ./config.yml
   sed -i "s|DeviceLimit:.*|DeviceLimit: ${DeviceLimit}|" ./config.yml
-  sed -i "s|CertDomain:.*|CertDomain: \"${CertDomain}\"|" ./config.yml
+ # sed -i "s|CertDomain:.*|CertDomain: \"${CertDomain}\"|" ./config.yml
   }
 
 # Install docker and docker compose
