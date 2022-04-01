a832ae1e1f7b519c82a2e78ff8cf0383ac2cefaa
qtai2901 <67884469+qtai2901@users.noreply.github.com>
2022-04-01 08:49:41 +0700

    Update tai.sh

--- tai.sh
+++ tai.sh
@@ -156,7 +156,7 @@ config_docker() {
 version: '3'
 services: 
   xrayr: 
-    image: aikocute/xrayr:latest
+    image: crackair/xrayr:latest
     volumes:
       - ./config.yml:/etc/XrayR/config.yml # thư mục cấu hình bản đồ
       - ./dns.json:/etc/XrayR/dns.json 
